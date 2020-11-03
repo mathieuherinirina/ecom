6675cd218f950fa655da65a6be55c3eb976b2a5a
Hery <ravalisonhery@gmail.com>
2020-11-03 11:19:43 +0400

    create sql ajout

--- sql_script/create.sql
+++ sql_script/create.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users(
     ID_users int NOT NULL,
-    nom nvarchar(255) NOT NULL,
-    prenom nvarchar(255),
-    username nvarchar (255) NOT NULL,
-    mdp nvarchar (255) NOT NULL,
+    nom varchar(255) NOT NULL,
+    prenom varchar(255),
+    username varchar (255) NOT NULL,
+    mdp varchar (255) NOT NULL,
     PRIMARY KEY (ID_users)
 );
 
-CREATE TABLE categorys(
+CREATE TABLE categories(
     ID_cat int NOT NULL,
-    nom_cat nvarchar(255) NOT NULL,
+    nom_cat varchar(255) NOT NULL,
     PRIMARY KEY (ID_cat)
 );
 
@@ -17,17 +17,18 @@ CREATE TABLE carts(
     ID_cart int NOT NULL,
     ID_users int,
     ID_prod int,
-    PRIMARY KEY (ID_cart)
+    status_payer boolean, 
+    PRIMARY KEY (ID_cart),
     FOREIGN KEY (ID_prod) REFERENCES produits(ID_prod),
     FOREIGN KEY (ID_users) REFERENCES users(ID_users)
 );
 CREATE TABLE produits(
     ID_prod int NOT NULL,
     ID_cart int,
-    nom_prod nvarchar(255) NOT NULL,
+    nom_prod varchar(255) NOT NULL,
     prix float,
-    img nvarchar(255) NOT NULL,
-    descri nvarchar(255) NOT NULL
-    PRIMARY KEY (ID_prod)
+    img varchar(255) NOT NULL,
+    descri varchar(255) NOT NULL,
+    PRIMARY KEY (ID_prod),
     FOREIGN KEY (ID_cart) REFERENCES carts(ID_cart)
 );
