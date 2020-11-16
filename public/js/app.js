c4bd2fdef05bf6e69747f342089336c2a4e3d9e2
Hery <ravalisonhery@gmail.com>
2020-11-16 09:08:39 +0400

    sary

--- public/js/app.js
+++ public/js/app.js
@@ -19391,8 +19391,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\ProgramLogi\wamp\www\ecom\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\ProgramLogi\wamp\www\ecom\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\wamp64\www\ecom\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\wamp64\www\ecom\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
